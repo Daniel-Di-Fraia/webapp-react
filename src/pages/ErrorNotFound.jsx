4fcb427648e8b28371d08f837fc5a43655023bdf
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-11-04 17:34:05 +0100

    refactoring + css

--- src/pages/ErrorNotFound.jsx
+++ src/pages/ErrorNotFound.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 const ErrorNotFound = () => {
     return (
         <div className="vh-100 d-flex flex-column align-items-center justify-content-center">
-            <h2 className="text-white fs-1">Page Not Found</h2>
-            <p className="my-3 text-white fs-3">Sorry, but the page you are looking for does not exist.</p>
-            <Link className="btn btn-warning fs-4" to="/">Back to home</Link>
+            <h2 className="text-white fs-1">Pagina non trovata</h2>
+            <p className="my-3 text-white fs-3">Ci dispiace, ma la pagina che stai cercando non esiste</p>
+            <Link className="btn btn-warning fs-4" to="/">Torna alla Home</Link>
         </div>
     )
 }
